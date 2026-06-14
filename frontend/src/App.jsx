d46d237edd44cf046fb0658eda71f0a4b07c6863
AryanCodeWizard <contact.aryan001@gmail.com>
2026-06-14 15:17:27 +0530

    feat: initial project scaffold — React client, Express server, Prisma ORM

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+const App = () => {
+  return (
+   <div className="bg-black text-white flex min-h-screen items-center justify-center text-3xl font-bold">
+  Hey there
+</div>
+  )
+}
+
+export default App
